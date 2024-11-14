b7d2e3f5eb9d158826bec62bb30395cfbe538fd2
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-11-14 13:08:02 +0530

    Update Python version to 3.9.20 in data ingestion, model training, and model evaluation notebooks

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ url = "https://sahilraja-kidney-disease-classification.hf.space/predict/file"
 
 
 # Set the file path
-file_path = "artifacts/data_ingestion/content/data/CT KIDNEY DATASET Normal, CYST, TUMOR and STONE/CYST/Cyst- (20).jpg"
+file_path = "artifacts/data_ingestion/content/data/CT KIDNEY DATASET Normal, CYST, TUMOR and STONE/STONE/Stone- (24).jpg"
 
 # Send the POST request with the image file
 with open(file_path, "rb") as image_file:
